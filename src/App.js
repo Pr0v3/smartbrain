40b32ee97e2a54b75b8500097be929366a724820
unknown <cobsmr@gmail.com>
2019-12-28 06:03:51 +0700

    updating file path Logo.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navigation from "./components/navigation/Navigation";
-import Logo from "./components/navigation/Logo";
+import Logo from "./components/Logo/Logo";
 import "./App.css";
 
 function App() {
