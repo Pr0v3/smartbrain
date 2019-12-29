55b1d1a3411fc045bf6baa6f151cc616fa17912c
unknown <cobsmr@gmail.com>
2019-12-30 03:05:58 +0700

    adding component>FaceRecognition.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import "./App.css";
@@ -62,7 +63,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*<FaceRecognition />} */}
+        <FaceRecognition />}
       </div>
     );
   }
