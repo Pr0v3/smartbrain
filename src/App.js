67161deeebc8a8d804292ed4d78726be8093c414
unknown <cobsmr@gmail.com>
2019-12-30 02:30:27 +0700

    import clarifai API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "f523faf406eb4953b88ff3331956d210"
+});
 
 const particlesOptions = {
   particles: {
@@ -29,6 +34,22 @@ class App extends Component {
 
   onInputChange = event => {
     console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        // URL
+        "https://samples.clarifai.com/metro-north.jpg"
+      )
+      .then(
+        function(response) {
+          // do something with responseconsole.log(response);
+        },
+        function(err) {}
+      );
   };
   render() {
     return (
